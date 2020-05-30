fa1dacc9fba91dfcc61a6bbc6d80e0d5cca13f44
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2020-05-31 02:00:32 +0530

    added dropdown menu , added logo and heading on header ,added second page bg

---
+++ script/script.js
@@ -0,0 +1,20 @@
+var our_services=document.querySelector('.our-services');
+var dropdown_services=document.querySelector('.dropdown-services');
+our_services.addEventListener('mouseover',function(){
+    console.log("hi");
+    dropdown_services.style.display="flex";
+   
+});
+
+
+
+dropdown_services.addEventListener('mouseover',function(){
+    console.log("hi");
+    dropdown_services.style.display="flex";
+   
+});
+dropdown_services.addEventListener('mouseout',function(){
+    console.log("hi");
+    dropdown_services.style.display="none";
+   
+});
